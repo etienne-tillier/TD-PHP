fc75426de8f252de0ee76b8634c8bd1a213ac00a
Etienne Tillier <etienne.tillier@etu.umontpellier.fr>
2020-09-11 16:46:40 +0200

    TestVoiture en cours

---
+++ TD1/testVoiture.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>TestVoiture</title>
+</head>
+<body>
+	<?php 
+	require_once("voiture.php");
+	$voiture1 = new voiture("Mercedes","rouge","784dz87z");
+	 ?>
+</body>
+</html>
